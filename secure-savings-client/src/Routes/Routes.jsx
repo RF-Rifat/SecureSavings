8c243f823c4a48fe52d17b73b6dd84696f613dd1
RF-Rifat <rfrifat6344@gmail.com>
2024-01-16 01:48:22 +0600

    fornt end routes and basic signUp and login Page page added

--- secure-savings-client/src/Routes/Routes.jsx
+++ secure-savings-client/src/Routes/Routes.jsx
@@ -2,12 +2,17 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "./Root";
 import Login from "../Authentication/Login/Login";
 import SignUp from "../Authentication/SignUp";
+import Home from "../pages/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
     children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
       {
         path: "/login",
         element: <Login></Login>,
