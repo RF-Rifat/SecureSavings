4c594b26a71efd51c20e8e4c5071a485006ca05e
RF-Rifat <rfrifat6344@gmail.com>
2024-01-16 00:21:38 +0600

    material tailwind config file added

--- secure-savings-clint/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-import withMT from "@material-tailwind/html/utils/withMT";
-
-export default withMT({
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-});
