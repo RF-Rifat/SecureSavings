cedd78e4c1aecfe61d492eb045f922e8d10e25b5
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-01-16 19:05:44 +0600

    Added error page of the project

--- secure-savings-client/src/Routes/Routes.jsx
+++ secure-savings-client/src/Routes/Routes.jsx
@@ -3,11 +3,13 @@ import Login from "../Authentication/Login/Login";
 import SignUp from "../Authentication/SignUp";
 import Home from "../pages/Home";
 import Root from "./Root";
+import ErrorPage from "../pages/ErrorPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
