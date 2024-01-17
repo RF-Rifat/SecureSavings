4cb5f904953c08df327463806095a504db79c65b
Md Rabby <ablazerabbi159@gmail.com>
2024-01-17 14:23:02 +0600

    slider add complete

--- secure-savings-client/src/pages/Home.jsx
+++ secure-savings-client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import MarqueeComp from "./MarqueeComp";
 import EmiCalculator from "../components/Home/EmiCalculator";
 import Hero from "../components/Home/Hero";
+import Banner from "../components/Home/Banner/Banner";
 
 const Home = () => {
   return (
     <div>
+      <Banner></Banner>
       <Hero />
       <MarqueeComp></MarqueeComp>
       <EmiCalculator />
