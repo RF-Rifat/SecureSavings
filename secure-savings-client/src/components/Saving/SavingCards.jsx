6ebe2c3897ffced7375f050aeeeba88250779abb
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-01-17 14:25:13 +0600

    Added fremar motion animation

--- secure-savings-client/src/components/Saving/SavingCards.jsx
+++ secure-savings-client/src/components/Saving/SavingCards.jsx
@@ -5,7 +5,7 @@ const SavingCards = () => {
     const [card, setCard] = useState(6);
 
     return (
-        <div className="max-w-screen-2xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-10">
+        <div className="max-w-screen-2xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-5 lg:grid-cols-3 my-10">
             {[...Array(card)].map((_, index) => (
                 <SavingCard key={index} />
             ))}
