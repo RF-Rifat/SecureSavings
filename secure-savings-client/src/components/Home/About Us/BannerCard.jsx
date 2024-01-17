1f008a9395bca6f0f0fe9aaa79c03f58cc7893db
RF-Rifat <rfrifat6344@gmail.com>
2024-01-17 21:24:47 +0600

    working on banner card section

--- secure-savings-client/src/components/Home/About Us/BannerCard.jsx
+++ secure-savings-client/src/components/Home/About Us/BannerCard.jsx
@@ -4,7 +4,7 @@ const BannerCard = () => {
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap">
           <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
-            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+            <div className="relative flex flex-col min-w-0 break-words bg-gray-200 w-full mb-8 shadow-xl rounded-lg transition duration-300 ease-in-out transform hover:scale-105">
               <div className="px-4 py-5 flex-auto">
                 <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400"></div>
                 <h6 className="text-xl font-semibold">Awarded Agency</h6>
@@ -15,6 +15,7 @@ const BannerCard = () => {
               </div>
             </div>
           </div>
+
           <div className="w-full md:w-4/12 px-4 text-center">
             <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
               <div className="px-4 py-5 flex-auto">
--- secure-savings-client/src/pages/AboutUs.jsx
+++ secure-savings-client/src/pages/AboutUs.jsx
@@ -8,7 +8,7 @@ const AboutUs = () => {
         <section className="relative py-20 bg-blue-gray-500">
           <div
             className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20"
-            style={{ transform: "translateZ(0px)" }}
+            style={{ transform: "translateZ(400px)" }}
           >
             <svg
               className="absolute bottom-0 overflow-hidden"
