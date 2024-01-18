552a001ea2d371e0772265d57d2a3dd2315639bb
sadia11-a <sadiasorna96@gmail.com>
2024-01-18 19:31:44 +0600

    design responsive login page

--- secure-savings-client/src/Authentication/Login/Login.jsx
+++ secure-savings-client/src/Authentication/Login/Login.jsx
@@ -11,41 +11,53 @@ import {
 
 const Login = () => {
   return (
-    <Card className="w-96 mx-auto mt-11">
-      <CardHeader
-        variant="gradient"
-        color="gray"
-        className="mb-4 grid h-28 place-items-center"
-      >
-        <Typography variant="h3" color="white">
-          Login
-        </Typography>
-      </CardHeader>
-      <CardBody className="flex flex-col gap-4">
-        <Input label="Email" size="lg" />
-        <Input label="Password" size="lg" />
-        <div className="-ml-2.5">
-          <Checkbox label="Remember Me" />
-        </div>
-      </CardBody>
-      <CardFooter className="pt-0">
-        <Button variant="gradient" fullWidth>
-          Login
-        </Button>
-        <Typography variant="small" className="mt-6 flex justify-center">
-          Don&apos;t have an account?
-          <Typography
-            as="a"
-            href="/signUp"
-            variant="small"
-            color="blue-gray"
-            className="ml-1 font-bold"
-          >
-            Sign up
+    <div className="flex flex-col md:flex-row justify-center items-center lg:h-screen">
+      {/* Left Side: Image */}
+      <div className="w-full md:w-1/2 mb-6 md:mb-0">
+        <img
+          src="https://i.ibb.co/MBhxsyT/open-banking-platform-online-banking-system-finance-digital-transformation-concept-688351-161.jpg"
+          alt="Left Side Image"
+          className="w-full h-full object-cover"
+        />
+      </div>
+
+      {/* Right Side: Login Card */}
+      <Card className="w-full md:w-96 mb-14 mt-14 md:ml-6">
+        <CardHeader
+          variant="gradient"
+          color="gray"
+          className="mb-4 grid h-28 place-items-center"
+        >
+          <Typography variant="h3" color="white">
+            Login
           </Typography>
-        </Typography>
-      </CardFooter>
-    </Card>
+        </CardHeader>
+        <CardBody className="flex flex-col gap-4">
+          <Input label="Email" size="lg" />
+          <Input label="Password" size="lg" />
+          <div className="-ml-2.5">
+            <Checkbox label="Remember Me" />
+          </div>
+        </CardBody>
+        <CardFooter className="pt-0">
+          <Button variant="gradient" fullWidth>
+            Login
+          </Button>
+          <Typography variant="small" className="mt-6 flex justify-center">
+            Don&apos;t have an account?
+            <Typography
+              as="a"
+              href="/signUp"
+              variant="small"
+              color="blue-gray"
+              className="ml-1 font-bold"
+            >
+              Sign up
+            </Typography>
+          </Typography>
+        </CardFooter>
+      </Card>
+    </div>
   );
 };
 
