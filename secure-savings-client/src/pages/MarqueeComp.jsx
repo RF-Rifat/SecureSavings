65c0f7e12b6b7cfa3fb6ed3190bb1288cb820c24
rexaulrana <rexaul.rana@gmail.com>
2024-01-18 23:06:01 +0600

    toggle themeadded

--- secure-savings-client/src/pages/MarqueeComp.jsx
+++ secure-savings-client/src/pages/MarqueeComp.jsx
@@ -9,16 +9,11 @@ const MarqueeComp = () => {
     <div className="h-44 max-w-screen-2xl mx-auto">
       <h1 className="text-center text-4xl font-medium">Our partner</h1>
       <div>
-        <Marquee
-          className="mt-5"
-          gradient={true}
-          autoFill={true}
-          pauseOnHover={true}
-        >
-          <img className="h-40 px-5" src={visa} alt="" />
+        <Marquee className="mt-5" autoFill={true} pauseOnHover={true}>
+          <img className="h-36 px-5 " src={visa} alt="" />
           <img className="h-36 px-5" src={masterCard} alt="" />
           <img className="h-36 px-5" src={paypal} alt="" />
-          <img className="h-28 px-5" src={gPay} alt="" />
+          <img className="h-[145px] px-5" src={gPay} alt="" />
         </Marquee>
       </div>
     </div>
--- secure-savings-client/tailwind.config.js
+++ secure-savings-client/tailwind.config.js
@@ -1,6 +1,7 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
+  darkMode: "class",
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
@@ -14,5 +15,8 @@ module.exports = withMT({
       },
     },
   },
-  plugins: [],
+  // plugins: [require("daisyui")],
+  // daisyui: {
+  //   themes: ["light", "coffee"],
+  // },
 });
