9175250bae89b6b3c7d6c6d3155e507ff96a21c7
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-01-26 15:52:39 +0600

    Updated online banking section and latest blogs section

--- secure-savings-client/src/components/Home/Features.jsx
+++ secure-savings-client/src/components/Home/Features.jsx
@@ -13,11 +13,11 @@ const Features = () => {
   return (
     <div className="px-2 mt-32">
       <div className="text-center mt-20">
-        
+
         <div className="px-4 md:text-center">
           <h2 className="py-2 text-2xl font-semibold text-gray-800 md:text-4xl dark:text-gray-300">
-          Online Banking at your <br /> fingerprints
-            
+            Online Banking at your <br /> fingerprints
+
           </h2>
           <div className="flex w-32 mt-1 overflow-hidden rounded mx-auto md:mb-14">
             <div className="flex-1 h-2 bg-teal-200"></div>
@@ -35,7 +35,9 @@ const Features = () => {
                 <div className="container">
                   <div
                     className="card dark:!bg-dark"
-                    style={{ "--clr": "#009688" }}
+                    style={{
+                      "--clr": "#009688",
+                    }}
                   >
                     <div className="img-box">
                       <img
