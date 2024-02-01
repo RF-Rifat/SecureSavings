13a615af8e677f52c965bdde01acc343686102d8
RF-Rifat <rfrifat6344@gmail.com>
2024-02-01 14:08:50 +0600

    user register schema updated

--- Secure-Savings-Server/index1.js
+++ Secure-Savings-Server/index1.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5001;
 
 // connect mongodb configuration using mongoose
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(`mongodb+srv://secure-savings:${process.env.DB_PASS}@cluster0.tryvron.mongodb.net/?retryWrites=true&w=majority`)
   .then(console.log("Mongooses Connected Successfully"))
   .catch((error) => console.log("Error connecting to MongoDB", error));
 
