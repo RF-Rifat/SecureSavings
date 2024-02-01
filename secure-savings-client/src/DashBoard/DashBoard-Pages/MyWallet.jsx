e8632700836e45f926e7f9a1c9d617b8fbbb6e08
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-02-01 12:13:41 +0600

    Wallet section updated

--- secure-savings-client/src/DashBoard/DashBoard-Pages/MyWallet.jsx
+++ secure-savings-client/src/DashBoard/DashBoard-Pages/MyWallet.jsx
@@ -6,16 +6,22 @@ const MyWallet = () => {
   const { LoggedUser } = authInfo;
   // , isAdmin
 
-  return (
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target;
+    const idNumber = form.idNumber.value;
+    const money = form.money.value;
+    console.log(idNumber, money);
 
+  }
+
+  return (
     <>
-      {/* user credit details */}
-      <div className="py-7 px-7 flex w-full gap-7 transition-all duration-1000 ease-in-out ">
+      <div className="py-7 flex w-full gap-7 transition-all duration-1000 ease-in-out ">
         <div className="flex flex-col w-full gap-7">
           <div className="bg-gradient-to-tl flex flex-col md:flex-row rounded-xl dark:bg-dark p-5 relative overflow-hidden">
-
-            <div className="flex flex-1 gap-4 sm:flex-wrap ssm:gap-12 xl:gap-6 flex-col xl:flex-row xl:justify-between xl:items-end lg:h-fit">
-              <div className="flex flex-col gap-3">
+            <div className="flex flex-1 gap-4 sm:flex-wrap sm:gap-12 xl:gap-6 flex-col lg:flex-row">
+              <div className="flex-1 flex-col gap-3 justify-center items-center">
                 <span className="text-[32px] font-bold text-dark dark:text-white sm:text-[40px] lg:text-[36px] xl:text-[40px]">
                   Your Balance: $00
                 </span>
@@ -35,7 +41,7 @@ const MyWallet = () => {
                       alt=""
                     />
                   </li>
-                  <li className="ml-5 mb-5 md:mb-0">
+                  <li className="pb-5 md:mb-0">
                     <img
                       className="w-auto object-cover mx-auto h-36"
                       src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/mastercard-id-check.png"
@@ -44,30 +50,27 @@ const MyWallet = () => {
                   </li>
                 </ul>
               </div>
-            </div>
-            {/* Input Field */}
-            <div className="w-full px-4 lg:w-1/2 xl:w-5/12">
-              <div className="mx-4">
-                <div
-                  className="credit-card w-full sm:w-auto shadow-lg mx-auto rounded-xl"
-                >
-                  <header className="flex flex-col justify-center items-center">
-                    <div
-                      className="relative"
-                    >
-                      <img
-                        className="w-full h-auto"
-                        src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/svg-cards/card-visa-front.png"
-                        alt="front credit card"
-                      />
-                    </div>
-
-                  </header>
-                  <main className="p-4">
-                    <div>
+              <div className="flex-1 w-full px-4 lg:w-1/2 xl:w-5/12">
+                <div className="mx-4">
+                  <div
+                    className="credit-card w-full sm:w-auto shadow-lg mx-auto rounded-xl"
+                  >
+                    <header className="flex flex-col justify-center items-center">
+                      <div
+                        className="relative"
+                      >
+                        <img
+                          className="w-full h-auto"
+                          src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/svg-cards/card-visa-front.png"
+                          alt="front credit card"
+                        />
+                      </div>
+                    </header>
+                    <form className="p-4" onSubmit={handleSubmit}>
                       <div className="my-3">
                         <input
                           type="text"
+                          name="idNumber"
                           className="block w-full px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
                           placeholder="Card number"
                           maxLength={19}
@@ -75,26 +78,25 @@ const MyWallet = () => {
                       </div>
                       <div className="my-3">
                         <input
-                          type="text"
+                          type="number"
+                          name="money"
                           className="block w-full px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
                           placeholder="Money"
                           maxLength={22}
                         />
                       </div>
-                    </div>
-                  </main>
-                  <footer className="px-4 pb-4">
-                    <button
-                      className="submit-button px-4 py-3 rounded-full bg-blue-300 text-blue-900 focus:ring focus:outline-none w-full text-xl font-semibold transition-colors"
-                    >
-                      Send Money
-                    </button>
-                  </footer>
+                      <footer className="px-4 pb-4">
+                        <button
+                          className="submit-button px-4 py-3 rounded-full bg-[#2195f3] text-white focus:ring focus:outline-none w-full text-xl font-semibold transition-colors"
+                        >
+                          Send Money
+                        </button>
+                      </footer>
+                    </form>
+                  </div>
                 </div>
               </div>
-
             </div>
-
           </div>
         </div>
       </div>
