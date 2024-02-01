40c140d49fafaba7413fd08ab405085edbca745e
rexaulrana <rexaul.rana@gmail.com>
2024-02-01 19:14:32 +0600

    add blog form update

--- secure-savings-client/src/pages/blogs/AddBlog.jsx
+++ secure-savings-client/src/pages/blogs/AddBlog.jsx
@@ -1,6 +1,8 @@
 import toast from "react-hot-toast";
 import { modifyData } from "../../Hooks/Api";
 import { useContext } from "react";
+import { Input, Option, Select, Textarea } from "@material-tailwind/react";
+
 import { AuthProvider } from "../../Authentication/AuthProvider";
 const AddBlog = () => {
   const { user } = useContext(AuthProvider);
@@ -9,6 +11,7 @@ const AddBlog = () => {
     e.preventDefault();
 
     const form = e.target;
+    // const type = form.type.value;
     const currentDate = new Date();
     const formattedDate = currentDate.toDateString();
 
@@ -21,6 +24,7 @@ const AddBlog = () => {
       blogImage: form.image.value,
       post: form.post.value,
     };
+    // console.log(type);
 
     try {
       const res = await modifyData("/api/blog", "POST", newBlog);
@@ -35,36 +39,31 @@ const AddBlog = () => {
   };
   return (
     <div>
-      <div className="bg-blue-gray-300 dark:bg-dark border rounded-lg px-8 py-6 mx-auto my-8">
+      <div className="bg-blue-gray-100 dark:bg-dark border rounded-lg px-8 py-6 mx-auto my-8">
         <h2 className="text-2xl font-medium mb-4 text-center">Add blog</h2>
         <form onSubmit={handleNewBlog} className="md:px-32 lg:px-80">
           {/* 1st row */}
 
           <div className=" gap-5">
             <div className="mb-4">
-              <label className="block text-gray-700 font-medium mb-2">
-                Blog Title
-              </label>
-              <input
-                type="text"
-                placeholder="Blog title"
-                id="title"
+              <Input
+                className=""
                 name="title"
-                className="border  text-black border-gray-400 p-2 w-full rounded-lg focus:outline-none focus:border-blue-400"
+                variant="standard"
+                label="Blog Title "
+                placeholder="Type here.."
                 required
+                success
               />
             </div>
             <div className="mb-4">
-              <label className="block text-gray-700 font-medium mb-2">
-                Blog Image
-              </label>
-              <input
-                type="text"
-                placeholder="Photo link"
-                id="image"
+              <Input
                 name="image"
-                className="border text-black border-gray-400 p-2 w-full rounded-lg focus:outline-none focus:border-blue-400"
+                variant="standard"
+                label="Image Link"
+                placeholder="Type here.."
                 required
+                success
               />
             </div>
           </div>
@@ -72,33 +71,49 @@ const AddBlog = () => {
           <div className="">
             {" "}
             <div className="mb-4">
-              <label className="block text-gray-700 font-medium mb-2">
-                Blog Type
-              </label>
-              <select
+              <Select
+                id="type"
+                name="type"
+                variant="standard"
+                label="Select blog type"
+                success
+              >
+                <Option value="Finance">Finance</Option>
+                <Option value="Banking">Banking</Option>
+                <Option value="Remittance">Remittance</Option>
+                <Option value="Load">Loan</Option>
+                <Option value="Bank Account">Bank Account</Option>
+              </Select>
+
+              {/* <select
                 id="type"
                 name="type"
                 className="border w-full border-gray-400 text-black p-2  rounded-lg focus:outline-none focus:border-blue-400"
-                required
               >
                 <option value="default">Select blog type</option>
                 <option value="Finance">Finance</option>
                 <option value="Banking">Banking</option>
                 <option value="Remittance">Remittance</option>
-              </select>
+              </select> */}
             </div>
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700 font-medium mb-2">
-              Write your blog
-            </label>
-            <textarea
+            <Textarea
+              success
+              name="post"
+              variant="standard"
+              label="Write your post "
+              placeholder="write here..."
+              required
+            />
+
+            {/* <textarea
               id="post"
               name="post"
               className="border text-black border-gray-400 p-2  w-full rounded-lg focus:outline-none focus:border-blue-400"
               rows="4"
               cols="4"
-            ></textarea>
+            ></textarea> */}
           </div>
 
           <div>
