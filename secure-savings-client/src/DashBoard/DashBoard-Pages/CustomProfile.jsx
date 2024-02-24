7be938da386444ca7f5a1c0bcb3242d07ea60a1a
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-02-24 19:32:20 +0600

    Show modal

--- secure-savings-client/src/DashBoard/DashBoard-Pages/CustomProfile.jsx
+++ secure-savings-client/src/DashBoard/DashBoard-Pages/CustomProfile.jsx
@@ -6,7 +6,7 @@ import BatteryStatus from "../../components/DashBoard/BatteryStatus";
 
 export function CustomProfile() {
   const { LoggedUser, isAdmin, userAcc } = useContext(AdminDataContext);
-  const { email, name, imageSrc } = LoggedUser[0] || {}
+  const { email, name, imageSrc } = LoggedUser[0] || {};
 
   // Modal
   const [showModal, setShowModal] = useState(false);
@@ -32,7 +32,7 @@ export function CustomProfile() {
 
   return (
     <>
-      <div className="relative flex flex-col bg-clip-border rounded-xl  bg-white text-gray-700 shadow-md mx-3 mb-6 lg:mx-4 border border-blue-gray-100">
+      <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md mx-3 mb-6 lg:mx-4 border border-blue-gray-100">
         <div className="p-4">
           <div className="mb-10 flex items-center justify-between flex-wrap gap-6">
             <div className="flex items-center gap-6">
@@ -207,18 +207,167 @@ export function CustomProfile() {
                   </a>
 
                   {showModal && (
-                    <div className="fixed top-0 z-20 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
-                      <div className="bg-white p-8 rounded-lg">
-                        <h2 className="text-lg font-bold mb-4">
+                    <div className="fixed top-0 z-50 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
+                      <div className="bg-white dark:bg-dark p-8 rounded-lg">
+                        <h2 className="text-lg font-bold mb-4 dark:text-white">
                           Activate Deactivated Card
                         </h2>
-                        <input
+                        {/* <input
                           type="text"
                           placeholder="Enter card number"
                           value={inputValue}
                           onChange={handleInputChange}
                           className="border border-gray-300 p-2 mb-4 rounded-md"
-                        />
+                        /> */}
+                        <form>
+                          <div className="grid gap-6 mb-6 md:grid-cols-2">
+                            <div>
+                              <label
+                                htmlFor="first_name"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                              >
+                                First name
+                              </label>
+                              <input
+                                type="text"
+                                id="first_name"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="John"
+                                required=""
+                              />
+                            </div>
+                            <div>
+                              <label
+                                htmlFor="last_name"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                              >
+                                Last name
+                              </label>
+                              <input
+                                type="text"
+                                id="last_name"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Doe"
+                                required=""
+                              />
+                            </div>
+                            <div>
+                              <label
+                                htmlFor="company"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                              >
+                                Company
+                              </label>
+                              <input
+                                type="text"
+                                id="company"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Flowbite"
+                                required=""
+                              />
+                            </div>
+                            <div>
+                              <label
+                                htmlFor="phone"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                              >
+                                Phone number
+                              </label>
+                              <input
+                                type="tel"
+                                id="phone"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="123-45-678"
+                                pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+                                required=""
+                              />
+                            </div>
+                            <div>
+                              <label
+                                htmlFor="website"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                              >
+                                Website URL
+                              </label>
+                              <input
+                                type="url"
+                                id="website"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="flowbite.com"
+                                required=""
+                              />
+                            </div>
+                            <div>
+                              <label
+                                htmlFor="visitors"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                              >
+                                Unique visitors (per month)
+                              </label>
+                              <input
+                                type="number"
+                                id="visitors"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder=""
+                                required=""
+                              />
+                            </div>
+                          </div>
+                          <div className="mb-6">
+                            <label
+                              htmlFor="email"
+                              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                              Email address
+                            </label>
+                            <input
+                              type="email"
+                              id="email"
+                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                              placeholder="john.doe@company.com"
+                              required=""
+                            />
+                          </div>
+                          <div className="mb-6">
+                            <label
+                              htmlFor="password"
+                              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                              Password
+                            </label>
+                            <input
+                              type="password"
+                              id="password"
+                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                              placeholder="•••••••••"
+                              required=""
+                            />
+                          </div>
+                          <div className="flex items-start mb-6">
+                            <div className="flex items-center h-5">
+                              <input
+                                id="remember"
+                                type="checkbox"
+                                defaultValue=""
+                                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+                                required=""
+                              />
+                            </div>
+                            <label
+                              htmlFor="remember"
+                              className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                            >
+                              I agree with the{" "}
+                              <a
+                                href="#"
+                                className="text-blue-600 hover:underline dark:text-blue-500"
+                              >
+                                terms and conditions
+                              </a>
+                              .
+                            </label>
+                          </div>
+                        </form>
                         <div className="flex justify-end">
                           <button
                             onClick={handleCloseModal}
