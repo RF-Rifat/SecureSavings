5a584c4d66ec451966a67f77e92b950835fa4746
RF-Rifat <rfrifat6344@gmail.com>
2024-03-15 20:12:03 +0600

    cloudinary installed

--- secure-savings-client/src/pages/Contact.jsx
+++ secure-savings-client/src/pages/Contact.jsx
@@ -3,12 +3,13 @@ import ContactPageMap from "../components/Contact/ContactPageMap";
 import ContactPageSTeam from "../components/Contact/ContactPageSTeam";
 
 const Contact = () => {
-
-    return (
-        <div className="max-w-7xl mx-auto px-6 lg:px-0">
-            <ContactPage />
-        </div>
-    );
+  return (
+    <div className="max-w-7xl mx-auto px-6 lg:px-0">
+      <ContactPageMap />
+      <ContactPageSTeam />
+      <ContactPage />
+    </div>
+  );
 };
 
 export default Contact;
